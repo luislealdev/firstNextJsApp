f6301fb4a749a0180c1de739fb7192c90f2c8b74
Luis Leal <luisrrleal@gmail.com>
2022-12-25 20:56:39 -0600

    Add navbar

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import { Navbar } from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function aboutPage() {
@@ -11,6 +12,8 @@ export default function aboutPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Navbar/>
 
       <main className={styles.main}>
         <h1>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import { Navbar } from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function HomePage() {
@@ -11,6 +12,8 @@ export default function HomePage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Navbar/>
 
       <main className={styles.main}>
         <h1>
