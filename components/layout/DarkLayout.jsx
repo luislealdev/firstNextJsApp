109adf4945d15cb0944390715bf66a4b43bca7ed
Luis Leal <luisrrleal@gmail.com>
2022-12-26 19:00:51 -0600

    Add and configurate multiple layout

---
+++ components/layout/DarkLayout.jsx
@@ -0,0 +1,12 @@
+export const DarkLayout = ({ children }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: "grey",
+        padding: "10px",
+      }}
+    >
+        {children}
+    </div>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import { DarkLayout } from "../components/layout/DarkLayout";
 import { MainLayout } from "../components/layout/MainLayout";
 
 export default function HomePage() {
   return (
-    <MainLayout> 
-        <h1>
-            Go to <Link href="/about"> about </Link>
-          </h1>
-    </MainLayout>
+    <h1>
+      Go to <Link href="/about"> about </Link>
+    </h1>
   );
 }
+
+HomePage.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
